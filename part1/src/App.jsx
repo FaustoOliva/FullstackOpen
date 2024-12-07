b17c244276429fdf6f877ea8165a082a5ee3209d
Fausto Oliva <fausto.calavera@gmail.com>
2024-12-07 14:41:34 -0300

    Part b: JavaScript

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -15,7 +15,7 @@ const Content = (props) => {
 
 const Total = (props) => {
   return (
-    <p>Number of exercises: {props.total}</p>
+    <p>Number of exercises: {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
   )
 }
 
@@ -27,28 +27,30 @@ const Part = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)} />
-      </div>
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </>
   )
 }
 
