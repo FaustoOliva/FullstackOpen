5d859a2da466d6a9054992c8520aa15dd58ddf6e
Fausto Oliva <fausto.calavera@gmail.com>
2024-12-07 13:10:38 -0300

    Part a: introduction

---
+++ part1/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+    <Part name = {props.parts[0].name} exercises = {props.parts[0].exercises} />
+    <Part name = {props.parts[1].name} exercises = {props.parts[1].exercises} />
+    <Part name = {props.parts[2].name} exercises = {props.parts[2].exercises} />
+  </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises: {props.total}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)} />
+      </div>
+  )
+}
+
+export default App
